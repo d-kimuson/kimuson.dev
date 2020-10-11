9a077271ad714946918bb741b78ebf4e2ef93b7d
Kaito <mail.kaito03@gmail.com>
2020-10-11 18:36:49 +0900

    [update] 404 Notfoundページにサイドバーを追加した

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,7 @@ import { PageProps } from "gatsby"
 
 import Layout from "../components/templates/layout"
 import Head from "../components/templates/head"
+import Sidebar from "../components/templates/sidebar"
 
 const NotFoundPage: React.FC<PageProps> = () => {
   const title = `404: Not Found`
@@ -13,12 +14,15 @@ const NotFoundPage: React.FC<PageProps> = () => {
       <Head title={title} description={description} />
       <Layout>
         <div className="l-page-container">
-          <main role="main">
-            <section>
-              <h1 className="m-page-title">404 Not Found</h1>
-              <p>ページが見つかりません。</p>
-            </section>
-          </main>
+          <div className="l-main-wrapper">
+            <main role="main">
+              <section>
+                <h1 className="m-page-title">404 Not Found</h1>
+                <p>ページが見つかりません。</p>
+              </section>
+            </main>
+          </div>
+          <Sidebar bio commonSidebar />
         </div>
       </Layout>
     </>
