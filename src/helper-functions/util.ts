2d343622ea50cb178f4d03b114c504516eb0e83d
kimuson <dev.kimuson@gmail.com>
2020-11-16 09:57:54 +0900

    [refactor] コメントを削除

--- src/helper-functions/util.ts
+++ src/helper-functions/util.ts
@@ -1,8 +1,3 @@
-// export const replaceAll = (
-//   baseString: string,
-//   beforeString: string,
-//   afterString: string
-// ): string => baseString.replace(new RegExp(beforeString, `g`), afterString)
 export const replaceAll = (
   baseString: string,
   beforeString: string,
