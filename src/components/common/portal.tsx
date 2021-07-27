9570c6e4af85b0236554fa0a566299984295d72f
d-kimsuon <d-kimuson@gmail.com>
2021-07-28 08:20:51 +0900

    fix(portal): ビルド時は portal は不要

--- src/components/common/portal.tsx
+++ src/components/common/portal.tsx
@@ -3,7 +3,10 @@ import { createPortal } from "react-dom"
 type PortalProps = React.PropsWithChildren<Record<string, unknown>>
 
 export const Portal: React.VFC<PortalProps> = ({ children }: PortalProps) => {
-  const element = document.getElementById("layout-root")
+  const element =
+    typeof window !== "undefined"
+      ? document.getElementById("layout-root")
+      : null
 
   return element !== null ? createPortal(children, element) : null
 }
