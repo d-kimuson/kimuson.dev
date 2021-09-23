d44a06737a991dbb8389e95c0729c914bb2b09af
d-kimsuon <d-kimuson@gmail.com>
2021-09-23 16:35:59 +0900

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,39 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require("ts-jest/utils")
+const {
+  readConfigFile,
+  parseJsonConfigFileContent,
+  sys,
+} = require("typescript")
 
-const { compilerOptions } = require('./tsconfig.json')
+// require で拾うとコメントが syntax error になるので
+const configFile = readConfigFile("./tsconfig.json", sys.readFile)
+if (typeof configFile.error !== "undefined") {
+  throw new Error(`Failed to load tsconfig: ${configFile.error}`)
+}
+
+const { options } = parseJsonConfigFileContent(
+  configFile.config,
+  {
+    fileExists: sys.fileExists,
+    readFile: sys.readFile,
+    readDirectory: sys.readDirectory,
+    useCaseSensitiveFileNames: true,
+  },
+  __dirname
+)
 
 module.exports = {
-  roots: [
-    "<rootDir>"
-  ],
-  testMatch: [
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
+  roots: ["<rootDir>"],
+  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
   globals: {
     "ts-jest": {
-      tsconfig: "tests/tsconfig.json"
-    }
+      tsconfig: "tests/tsconfig.json",
+    },
   },
-  preset: "ts-jest",
-  moduleNameMapper: pathsToModuleNameMapper(
-    compilerOptions.paths , { prefix: "<rootDir>/" }
-  ),
+  transform: {
+    "^.+\\.(t|j)s$": "esbuild-jest",
+  },
+  moduleNameMapper: pathsToModuleNameMapper(options.paths, {
+    prefix: "<rootDir>/",
+  }),
 }
