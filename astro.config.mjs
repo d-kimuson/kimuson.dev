f146ef886f6dd60f6e4b0cbb97182d318633d069
d-kimsuon <dev.kimuson@gmail.com>
2023-09-23 13:30:29 +0900

    feat(*): 3系にあげた

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-// import { defaultLayoutPlugin } from "./default-layout-plugin.mjs";
 import remarkCodeTitles from "remark-code-titles";
 
 export default defineConfig({
