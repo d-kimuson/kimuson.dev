5152aa40ae61227cf6073cfe061fada9a7da80c8
d-kimsuon <dev.kimuson@gmail.com>
2025-01-05 17:39:50 +0900

    remove old blog

--- apps/blog/src/domain-object/heading.ts
+++
@@ -1,9 +0,0 @@
-import { object, number, integer, string, type Output } from "valibot";
-
-export const headingSchema = object({
-  depth: number([integer()]),
-  slug: string(),
-  text: string(),
-});
-
-export type Heading = Output<typeof headingSchema>;
--- apps/blog/src/env.d.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line
-/// <reference path="../.astro/types.d.ts" />
-// eslint-disable-next-line
-/// <reference types="astro/client" />
