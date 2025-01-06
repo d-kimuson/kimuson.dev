68d07bd8ac53b12db323701d5e14a896fc4f4060
d-kimsuon <dev.kimuson@gmail.com>
2025-01-07 03:26:42 +0900

    fix code generation step

--- scripts/deploy_on_cloudflare.sh
+++ scripts/deploy_on_cloudflare.sh
@@ -2,13 +2,18 @@
 
 set -eux
 
-printenv
-
 BUILD_ENV="production"
 if [[ "$CF_PAGES_BRANCH" != "master" ]]; then
   BUILD_ENV="preview"
 fi
 
-echo "Start build for $BUILD_ENV"
+echo "Start deploy script for $BUILD_ENV"
 
+echo "env:"
+printenv
+
+echo "Generate codes."
+pnpm --filter=@kimuson.dev/articles generate
+
+echo "build for $BUILD_ENV"
 ENV=$BUILD_ENV pnpm build
