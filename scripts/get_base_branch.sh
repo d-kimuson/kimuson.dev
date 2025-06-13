b0ec33da5a93a28c53084561b40f88fd13f6f028
d-kimsuon <dev.kimuson@gmail.com>
2025-06-14 00:38:37 +0900

    chore: add Claude Code

---
+++ scripts/get_base_branch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "${current_branch}" == "master" ]]; then
+  echo $current_branch
+else
+  git reflog --grep-reflog="to $()" | tac | head -n 1 | cut -d ' ' -f 6
+fi
